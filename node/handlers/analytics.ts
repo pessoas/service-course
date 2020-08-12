fe7c468ea7019d8fe4d2b455ca44aadb573d0f07
Renato Pessoa <renatopessoaneto@gmail.com>
2020-08-12 19:48:44 -0300

    analytics

--- node/handlers/analytics.ts
+++ node/handlers/analytics.ts
@@ -1,8 +1,12 @@
 export async function analytics(ctx: Context, next: () => Promise<any>) {
   if (ctx.method.toUpperCase() === 'GET') {
-    ctx.status = 200
-    ctx.body = 'OK'
 
+    const {
+      clients: { analytics },
+    } = ctx
+
+    ctx.status = 200
+    ctx.body = await analytics.getLiveUsers()
     ctx.set('cache-control', 'no-cache')
   }
   await next()
